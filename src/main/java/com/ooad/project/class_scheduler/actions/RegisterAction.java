9a50477a970a8165e1fd35cd4700e00c26b73e47
Sudharshan <sudhar.ti@gmail.com>
2014-10-04 14:17:49 -0500

    Add RegisterAction class

---
+++ src/main/java/com/ooad/project/class_scheduler/actions/RegisterAction.java
@@ -0,0 +1,15 @@
+package com.ooad.project.class_scheduler.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class RegisterAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public String execute() {
+		return SUCCESS;
+	}
+}
