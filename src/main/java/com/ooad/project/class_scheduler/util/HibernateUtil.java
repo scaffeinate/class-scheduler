1bb023accad4e07ffdb9b6ff25fa986f821da794
Sudharshan <sudhar.ti@gmail.com>
2014-10-05 14:28:35 -0500

    Add Hibernate Annmotations to pom.xml, add Hibernate Util

---
+++ src/main/java/com/ooad/project/class_scheduler/util/HibernateUtil.java
@@ -0,0 +1,32 @@
+package com.ooad.project.class_scheduler.util;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class HibernateUtil {
+
+	private static SessionFactory sessionFactory;
+	
+	public HibernateUtil(){
+		sessionFactory = constructSessionFactory();
+	}
+	
+	private static SessionFactory constructSessionFactory() {
+		try {
+			return new AnnotationConfiguration().configure().buildSessionFactory();
+		} catch (HibernateException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Hibernate SessionFactory construct Exception");
+		}
+		return sessionFactory;
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	
+	public static void closeFactory(){
+		sessionFactory.close();
+	}
+}
