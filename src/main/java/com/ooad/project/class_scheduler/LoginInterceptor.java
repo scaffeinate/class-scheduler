3bc091f99e1c00d514c71bfd2a7b6969ad7dbfcf
Sudharshan <sudhar.ti@gmail.com>
2014-10-05 03:00:00 -0500

    Add Interceptor

---
+++ src/main/java/com/ooad/project/class_scheduler/LoginInterceptor.java
@@ -0,0 +1,31 @@
+package com.ooad.project.class_scheduler;
+
+import org.apache.struts2.interceptor.I18nInterceptor;
+
+import com.opensymphony.xwork2.ActionInvocation;
+
+public class LoginInterceptor extends I18nInterceptor{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		// TODO Auto-generated method stub
+		return super.intercept(invocation);
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+}
