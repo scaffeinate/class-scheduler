32a151f716b8d36b24d311496027a190bbb9eab1
Sudharshan <sudhar.ti@gmail.com>
2014-10-05 17:27:37 -0500

    Added Mapping, changed hibernate config, schema changes

--- src/main/java/com/ooad/project/class_scheduler/bean/User.java
+++ src/main/java/com/ooad/project/class_scheduler/bean/User.java
@@ -1,47 +1,75 @@
 package com.ooad.project.class_scheduler.bean;
 
-public class User {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	
 	private int id;
 	private String name, email, password, username,
 		track, school;
 	
+	@Id
+	@Column(name="ID", unique=true, nullable=false)
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	@Column(name="NAME", unique=false, nullable=false, length=50)
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	@Column(name="EMAIL", unique=true, nullable=false, length=50)
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	@Column(name="PASSWORD", unique=false, nullable=false, length=100)
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	@Column(name="USERNAME", unique=true, nullable=false, length=25)
 	public String getUsername() {
 		return username;
 	}
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	
+	@Column(name="TRACK", unique=false, nullable=false, length=50)
 	public String getTrack() {
 		return track;
 	}
 	public void setTrack(String track) {
 		this.track = track;
 	}
+	
+	@Column(name="SCHOOL", unique=false, nullable=false, length=50)
 	public String getSchool() {
 		return school;
 	}
